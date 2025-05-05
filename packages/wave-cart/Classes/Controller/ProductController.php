b59a91fbff45d09974ecd5772bdca9334391a2be
Juliane Wundermann <j.wundermann@familie-redlich.de>
2025-05-05 13:31:24 +0200

    [TASK] add list and show action for ProductController

--- packages/wave-cart/Classes/Controller/ProductController.php
+++ packages/wave-cart/Classes/Controller/ProductController.php
@@ -2,7 +2,32 @@
 
 namespace TYPO3Incubator\WaveCart\Controller;
 
-class ProductController
-{
+use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
+use TYPO3Incubator\WaveCart\Domain\Model\Product;
+use TYPO3Incubator\WaveCart\Domain\Repository\ProductRepository;
 
+class ProductController extends ActionController
+{
+    public function __construct(
+        protected readonly ProductRepository $productRepository
+    )
+    {
+    }
+
+    /**
+     * Index Action
+     *
+     * @return string
+     */
+    public function listAction()
+    {
+        $products = $this->productRepository->findAll();
+        $this->view->assign('products', $products);
+        return $this->view->render();
+    }
+
+    public function showAction(Product $product)
+    {
+        $this->view->assign('product', $product);
+    }
 }
