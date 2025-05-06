033cd80626a2104d366f3bd6c9c2f42dbee9ad02
maximilian.schell <maximilian.schell@snk.de>
2025-05-06 17:31:32 +0100

    [TASK] Create order view and integrate data

--- packages/wave-cart/Classes/Controller/OrderController.php
+++ packages/wave-cart/Classes/Controller/OrderController.php
@@ -5,39 +5,24 @@ declare(strict_types=1);
 namespace TYPO3Incubator\WaveCart\Controller;
 
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
-use TYPO3Incubator\WaveCart\Domain\Repository\ProductRepository;
 use TYPO3Incubator\WaveCart\Domain\Repository\ProductVariantRepository;
 use Psr\Http\Message\ResponseInterface;
 use TYPO3Incubator\WaveCart\Dto\OrderDto;
 
 class OrderController extends ActionController
 {
-    private ProductRepository $productRepository;
     private ProductVariantRepository $productVariantRepository;
 
     public function __construct(
-        ProductRepository $productRepository,
         ProductVariantRepository $productVariantRepository
     ) {
-        $this->productRepository = $productRepository;
         $this->productVariantRepository = $productVariantRepository;
     }
 
-
-    /**
-     * Handles the cart action by retrieving the cart data from the session, preparing the cart items,
-     * and assigning them to the view for rendering.
-     *
-     * @return ResponseInterface The rendered HTML response containing the cart data.
-     */
     public function cartAction(): ResponseInterface
     {
-        //TODO get the cart from somewhere
-        $cart = [
-            'variant_id' => '1',
-            'variant_id' => '4',
-            'variant_id' => '6'
-        ];
+        //TODO get the cart from somewhere else
+        $cart = ['1', '4', '6'];
         $cartItems = $this->prepareCartItems($cart);
 
         $this->view->assignMultiple([
@@ -47,33 +32,35 @@ class OrderController extends ActionController
         return $this->htmlResponse();
     }
 
-    public function addCustomerDataAction(OrderDto $order)
+    public function addCustomerDataAction(OrderDto $order): ResponseInterface
     {
         $this->view->assign('order', $order);
+
         return $this->htmlResponse();
     }
 
-    /**
-     * Prepares the cart items by retrieving product and variant details.
-     *
-     * @param array $cart The cart data containing product and variant identifiers.
-     * @return array The prepared cart items with associated product and variant details.
-     */
     private function prepareCartItems(array $cart): array
     {
         $cartItems = [];
 
-        foreach ($cart as $key => $item) {
-            $product = $this->productRepository->findByIdentifier($item['productId']);
-            $variant = $item['variantId'] ? $this->productVariantRepository->findByIdentifier($item['variantId']) : null;
+        foreach ($cart as $key => $variantUid) {
+            $variant = $this->productVariantRepository->findByUid($variantUid);
+            if (!$variant) {
+                continue;
+            }
+
+            $product = $variant->getProduct();
+            if (!$product) {
+                continue;
+            }
 
             $cartItems[] = [
                 'key' => $key,
-                'product' => $product,
-                'variant' => $variant,
-                'variant_amount_available' => $variant->getAmount(),
-                'product_price' => $product->getPrice(),
-                'product_name' => $product->getPrice(),
+                'id' => $variant->getUid(),
+                'name' => $variant->getName(),
+                'amount' => $variant->getAmount(),
+                'size' => $variant->getSize(),
+                'price' => $product->getPrice(),
             ];
         }
 
