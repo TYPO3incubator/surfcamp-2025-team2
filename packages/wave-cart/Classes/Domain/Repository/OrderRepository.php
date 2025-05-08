fe98fe47aff22dc5aa2c742385b4ffbabdf45ab5
maximilian.schell <maximilian.schell@snk.de>
2025-05-09 00:32:40 +0100

    [TASK] Fix invoice template

--- packages/wave-cart/Classes/Domain/Repository/OrderRepository.php
+++ packages/wave-cart/Classes/Domain/Repository/OrderRepository.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace TYPO3Incubator\WaveCart\Domain\Repository;
 
+use TYPO3\CMS\Core\Database\Connection;
 use Doctrine\DBAL\Exception;
 use Doctrine\DBAL\Query\QueryBuilder;
 use Doctrine\DBAL\Result;
@@ -35,5 +36,24 @@ class OrderRepository extends Repository
     private function getQueryBuilder(string $table): QueryBuilder
     {
         return $this->connectionPool->getQueryBuilderForTable($table);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function getCreationDateByOrderId(int $orderId): ?int
+    {
+        $queryBuilder = $this->getQueryBuilder(self::TABLE_NAME);
+
+        $result = $queryBuilder
+            ->select('crdate')
+            ->from(self::TABLE_NAME)
+            ->where(
+                $queryBuilder->expr()->eq('uid', $queryBuilder->createNamedParameter($orderId, Connection::PARAM_INT))
+            )
+            ->executeQuery()
+            ->fetchAssociative();
+
+        return $result['crdate'] ?? null;
     }
 }
--- packages/wave-cart/Classes/Service/GenerateInvoiceService.php
+++ packages/wave-cart/Classes/Service/GenerateInvoiceService.php
@@ -2,30 +2,47 @@
 
 namespace TYPO3Incubator\WaveCart\Service;
 
+use TYPO3\CMS\Core\Resource\Exception\InsufficientFolderAccessPermissionsException;
 use TYPO3\CMS\Core\Resource\File;
 use TYPO3\CMS\Core\Resource\ResourceFactory;
 use TYPO3\CMS\Core\Resource\StorageRepository;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Utility\StringUtility;
+use Mpdf\Mpdf;
+use Mpdf\MpdfException;
 use TYPO3\CMS\Core\View\ViewFactoryData;
 use TYPO3\CMS\Core\View\ViewFactoryInterface;
 use TYPO3\CMS\Extbase\Mvc\Request;
 use TYPO3Incubator\WaveCart\Domain\Model\Order;
+use TYPO3Incubator\WaveCart\Domain\Repository\OrderRepository;
 
 class GenerateInvoiceService
 {
     public function __construct(
         private ViewFactoryInterface $viewFactory,
-    ) {}
+        protected OrderRepository $orderRepository,
+    ) {
+    }
 
+    /**
+     * @throws MpdfException
+     * @throws InsufficientFolderAccessPermissionsException
+     */
     public function generateInvoicePdfAndAddToOrder(Order $order, Request $request): Order
     {
         $settings = $request->getAttribute('site')->getSettings();
-        $company = $settings->get('waveCart.invoice.company');
+        $companyName = $settings->get('waveCart.invoiceCompanyName');
+        $companyStreet = $settings->get('waveCart.invoiceCompanyStreet');
+        $companyCity = $settings->get('waveCart.invoiceCompanyCity');
+        $companyPhone = $settings->get('waveCart.invoiceCompanyPhone');
+        $iban = $settings->get('waveCart.invoiceIban');
+        $bic = $settings->get('waveCart.invoiceBic');
+        $bankName = $settings->get('waveCart.invoiceBankName');
+        $creationDate = date('d.m.Y', $this->orderRepository->getCreationDateByOrderId($order->getUid()));
         $fileIdentifier = 'invoice-' . $order->getUid() . '.pdf';
         $tax = $this->calculateTax($order);
 
-        $mpdf = new \Mpdf\Mpdf([
+        $mpdf = new Mpdf([
             'margin_left' => 20,
             'margin_right' => 15,
             'margin_top' => 48,
@@ -34,9 +51,9 @@ class GenerateInvoiceService
             'margin_footer' => 10
         ]);
 
-        $mpdf->SetProtection(array('print'));
-        $mpdf->SetTitle($company . " - Invoice");
-        $mpdf->SetAuthor($company);
+        $mpdf->SetProtection(['print']);
+        $mpdf->SetTitle($companyName . " - Invoice");
+        $mpdf->SetAuthor($companyName);
         $mpdf->SetDisplayMode('fullpage');
 
         $viewFactoryData = new ViewFactoryData(
@@ -45,7 +62,15 @@ class GenerateInvoiceService
         );
         $view = $this->viewFactory->create($viewFactoryData);
         $view->assign('order', $order);
+        $view->assign('companyName', $companyName);
+        $view->assign('companyStreet', $companyStreet);
+        $view->assign('companyCity', $companyCity);
+        $view->assign('companyPhone', $companyPhone);
         $view->assign('tax', $tax);
+        $view->assign('iban', $iban);
+        $view->assign('bic', $bic);
+        $view->assign('bankName', $bankName);
+        $view->assign('creationDate', $creationDate);
         $mpdf->WriteHTML($view->render('Invoice.html'));
         $mpdf->OutputFile('/tmp/' . $fileIdentifier);
 
